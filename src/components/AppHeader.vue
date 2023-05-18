e5e4d4d0811e0c118dc91f9e95966a705d01fcfe
GianluGiarde <gianlugiarde@gmail.com>
2023-05-18 17:37:23 +0200

    fix: format

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,65 +1,65 @@
 <script></script>
 
 <template>
-    <header>
-        <div class="container">
-            <div class="logo">
-                <img src="./../assets/img/dc-logo.png" alt="">
-            </div>
-            <ul class="nav">
-                <li>charactes</li>
-                <li>comincs</li>
-                <li>movies</li>
-                <li>tv</li>
-                <li>games</li>
-                <li>collectibles</li>
-                <li>videos</li>
-                <li>fans</li>
-                <li>news</li>
-                <li>shop</li>
-            </ul>
-        </div>
-    </header>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="./../assets/img/dc-logo.png" alt="" />
+      </div>
+      <ul class="nav">
+        <li>charactes</li>
+        <li>comincs</li>
+        <li>movies</li>
+        <li>tv</li>
+        <li>games</li>
+        <li>collectibles</li>
+        <li>videos</li>
+        <li>fans</li>
+        <li>news</li>
+        <li>shop</li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
 .container {
-    width: 960px;
-    max-width: 1280px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: repeat(16, 1fr);
+  width: 960px;
+  max-width: 1280px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(16, 1fr);
 }
 
 .container {
-    .logo {
-        height: 88px;
-        grid-column: 1 / 2;
-        display: grid;
-        place-items: center;
-    }
+  .logo {
+    height: 88px;
+    grid-column: 1 / 2;
+    display: grid;
+    place-items: center;
+  }
 
-    .logo {
-        img {
-            display: block;
-            width: 60px;
-        }
+  .logo {
+    img {
+      display: block;
+      width: 60px;
     }
+  }
 
-    .nav {
-        height: 88px;
-        grid-column: 7 / 17;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-    }
+  .nav {
+    height: 88px;
+    grid-column: 7 / 17;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 
-    .nav {
-        li {
-            text-transform: uppercase;
-            font-size: 0.7rem;
-            font-weight: 600;
-        }
+  .nav {
+    li {
+      text-transform: uppercase;
+      font-size: 0.7rem;
+      font-weight: 600;
     }
+  }
 }
 </style>
