eeb026b04aec1229503c15e6d4099388d79700e4
GianluGiarde <gianlugiarde@gmail.com>
2023-05-19 01:48:40 +0200

    add: div in container

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,28 +1,34 @@
 <script></script>
 
 <template>
-    <main>
-        <div class="container-bg">
-            <div class="container">
-                Coming soon...
-            </div>
-        </div>
-    </main>
+  <main>
+    <div class="container-bg">
+      <div class="container">
+        <div>Coming soon...</div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 .container-bg {
-    background-color: #1C1C1C;
+  background-color: #1c1c1c;
 }
 
 .container {
-    width: 960px;
-    max-width: 1280px;
-    height: 120px;
-    margin: 0 auto;
+  width: 960px;
+  max-width: 1280px;
+  height: 100px;
+  margin: 0 auto;
+}
+
+.container {
+  div {
+    height: 100%;
     display: grid;
     align-content: center;
-    font-size: 2rem;
+    font-size: 2.5rem;
     color: white;
+  }
 }
 </style>
