c9409264a67cc7cacccb098dd2f7fdccb9fdc2b6
GianluGiarde <gianlugiarde@gmail.com>
2023-05-18 18:41:45 +0200

    add: middle-footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
   <footer>
-    <div class="container-bg">
+    <div class="top-footer-bg">
       <div class="container">
         <div class="top-footer">
           <section>
@@ -28,12 +28,70 @@
         </div>
       </div>
     </div>
+    <div class="middle-footer-bg">
+      <div class="container">
+        <div class="middle-footer">
+          <div class="lists">
+            <ul class="comics">
+              <li><h2>dc comics</h2></li>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+
+            <ul class="shop">
+              <li><h2>shop</h2></li>
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+
+            <ul class="dc">
+              <li><h2>dc</h2></li>
+              <li>Terms Of Use</li>
+              <li>Privacy policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contacs Us</li>
+            </ul>
+
+            <ul class="sites">
+              <li><h2>sites</h2></li>
+              <li>DC</li>
+              <li>MAD Magazines</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visas</li>
+            </ul>
+          </div>
+          <img src="./../assets/img/dc-logo-bg.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div class="bottom-footer-bg">
+      <div class="container">
+        <div class="bottom-footer"></div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-.container-bg {
+.top-footer-bg {
   background-color: #0282f9;
+}
+
+.middle-footer-bg {
+  background-image: url("./../assets/img/footer-bg.jpg");
 }
 
 .container {
@@ -70,6 +128,64 @@
       font-size: 0.7rem;
       text-transform: uppercase;
     }
+  }
+}
+
+.middle-footer {
+  height: 350px;
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  color: rgba(255, 255, 255, 0.4);
+  overflow: hidden;
+}
+
+.middle-footer {
+  .lists {
+    padding: 2rem 0 3rem;
+    grid-column: 1 / 4;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(7, 35px);
+    column-gap: 0.5rem;
+  }
+
+  .lists {
+    h2 {
+      color: white;
+      text-transform: uppercase;
+      font-size: 1rem;
+      margin-bottom: 0.7rem;
+    }
+
+    li {
+      line-height: 1.1rem;
+      font-size: 0.7rem;
+    }
+
+    .comics {
+      grid-column: 1 / 2;
+      grid-row: 1 / 6;
+    }
+
+    .shop {
+      grid-column: 1 / 2;
+      grid-row: 6 / 8;
+    }
+
+    .dc {
+      grid-column: 2 / 3;
+    }
+
+    .sites {
+      grid-column: 3 / 4;
+    }
+  }
+
+  img {
+    grid-column: 5 / 10;
+    display: block;
+    height: 150%;
+    align-self: center;
   }
 }
 </style>
