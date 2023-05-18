32b38777317a7a74d6c9c77811e2b00bde4ac453
GianluGiarde <gianlugiarde@gmail.com>
2023-05-18 16:40:16 +0200

    add: appheader

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # 1.0 SetUp
 
-# 1.1 SetUp componenti
+- SetUp componenti
+- SetUp assets
 
-# 1.2 SetUp assets
+# 2.0 Header
 
-#
+- Scheletro HTML header
+- 2.1 stile header
+
+# 3.0 Body
+
+- Scheletro HTML body
+- 3.1 Stile body
+
+# 4.0 Footer
+
+- Scheletro HTML footer
+- 4.1 Stile footer
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,70 @@
 <script></script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="./../assets/img/dc-logo.png" alt="">
+            </div>
+            <ul class="nav">
+                <li>charactes</li>
+                <li>comincs</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+    </header>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+header {
+    font-family: sans-serif;
+}
+
+.container {
+    width: 960px;
+    max-width: 1280px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(16, 1fr);
+}
+
+.container {
+    .logo {
+        height: 88px;
+        grid-column: 1 / 2;
+        display: grid;
+        place-items: center;
+    }
+
+    .logo {
+        img {
+            display: block;
+            width: 60px;
+        }
+    }
+
+    .nav {
+        height: 88px;
+        grid-column: 7 / 17;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        list-style-type: none;
+    }
+
+    .nav {
+        li {
+            text-transform: uppercase;
+            font-size: 0.7rem;
+            font-weight: 600;
+        }
+    }
+}
+</style>
