5da222ae07f60f804cb421f719beb3c0efc8e741
GianluGiarde <gianlugiarde@gmail.com>
2023-05-18 16:47:50 +0200

    add: appbody

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,5 +1,24 @@
 <script></script>
 
-<template></template>
+<template>
+    <main>
+        <div class="container">Coming soon...</div>
+    </main>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+main {
+    background-color: black;
+}
+
+.container {
+    width: 960px;
+    max-width: 1280px;
+    height: 120px;
+    margin: 0 auto;
+    display: grid;
+    align-content: center;
+    font-size: 2rem;
+    color: white;
+}
+</style>
