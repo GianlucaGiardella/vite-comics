1a1cae7525c4e76107b6261b64f5816b15a3410e
GianluGiarde <gianlugiarde@gmail.com>
2023-05-18 15:22:48 +0200

    new project

---
+++ README.md
@@ -0,0 +1,7 @@
+# 1.0 SetUp
+
+# 1.1 SetUp componenti
+
+# 1.2 SetUp assets
+
+#
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppBody from './components/Appbody.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  components: { AppHeader, AppBody, AppFooter }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppBody />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use './assets/styles/general.scss' as *;
+</style>
