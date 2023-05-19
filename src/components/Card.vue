c885135ca43a2de94f6e281b7b7306ff3b7fb9df
GianluGiarde <gianlugiarde@gmail.com>
2023-05-19 19:38:02 +0200

    add: card style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,4 +15,33 @@ export default {
   </li>
 </template>
 
-<style></style>
+<style lang="scss">
+li {
+  padding-inline: 0.7rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  text-align: start;
+}
+
+li {
+  img {
+    width: 100%;
+    height: 140px;
+    object-fit: cover;
+    object-position: top;
+  }
+
+  h2 {
+    font-size: 0.6rem;
+    font-weight: 400;
+    text-transform: uppercase;
+    line-height: 0.8rem;
+  }
+
+  img:hover,
+  h2:hover {
+    cursor: pointer;
+  }
+}
+</style>
